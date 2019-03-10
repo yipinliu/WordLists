908cb732954e75b4ef53cd8f33232d9d2e2de163
yipinliu <ypliu18@gmail.com>
2019-03-10 10:57:29 +0800

    Added common type

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include "type.hpp"
+#include "IO.hpp"
+#include "query.hpp"
+
+int main(int argc, char** argv){
+    /* maybe follow these step:
+    1. parse the command line;
+    2. get the data from input
+    3. get the query from input
+    4. call different query function based on query type
+    5. output
+    */
+}
--- src/type.hpp
+++ src/type.hpp
@@ -3,3 +3,20 @@
 ///This file contain common type used through this program
 ///
 ///////////////////////////////////////////////////
+#ifndef __TYPE_HPP__
+#define __TYPE_HPP__
+#include <list>
+struct Word{
+    char first;
+    char last;
+    int len;
+    char* raw;
+};
+
+struct ListArray{
+    std::list<Word*> hNoVisited;
+    std::list<Word*> hVisited;
+    std::list<Word*> tNoVisited;
+    std::list<Word*> tVisited;
+};
+#endif
