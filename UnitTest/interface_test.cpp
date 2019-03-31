f2e85e0ec19858be1a6e09970c5bb317663994e4
JayeWei <48397831+JayeWei@users.noreply.github.com>
2019-03-31 14:58:34 +0800

    Update interface_test.cpp

--- UnitTest/interface_test.cpp
+++ UnitTest/interface_test.cpp
@@ -5,24 +5,39 @@ char *result[100];
 char *r = "Algebra1Apple2Zoo1Elephant1Under1Fox1Dog1Moon1Leaf1Trick1Pseudopseudohypoparathyroidism";
 char *raw = "The ReaSons why<they DO so are asa follows$$Firstly!!they$hope1to&lOOk smart*special ? I < the FollOWs/*BEfore,copY,eco1gAlaXy(hell)12345Killer@Length]NEED,open4people%%?Union??valUEax:you:zero*/looK";
 
-TEST(MostCharacters, all)
+TEST(MostCharacter,rawTestCharacter)
 {
 	struct WordList *wordlist = getWords(raw);
 	std::list<std::list<Word*> >results;
 	long len = MostCharacters(wordlist, 0, 0, results);
 	EXPECT_EQ(len,64);
-
 }
-TEST(MostWords,all)
+TEST(MostCharacter,rTestCharacter)
+{
+	struct WordList *wordlist = getWords(r);
+	std::list<std::list<Word*> >results;
+	long len = MostCharacters(wordlist, 0, 'n', results);
+	EXPECT_EQ(len,34);
+}
+TEST(MostWords,rawTestWords1)
 {
 	struct WordList *wordlist = getWords(raw);
 	std::list<std::list<Word*> >results;
 	long len = MostWords(wordlist, 0, 0, results);
 	EXPECT_EQ(len,13);
-	len = MostWords(wordlist, 'h', 't' , results);
+}
+TEST(MostWords,rawTestWords2)
+{
+	struct WordList *wordlist = getWords(raw);
+	std::list<std::list<Word*> >results;
+	long len = MostWords(wordlist, 'h', 't', results);
 	EXPECT_EQ(len,5);
-	wordlist = getWords(r);
-	len = MostWords(wordlist, 0, 't', results);
+}
+TEST(MostWords,rTestWords)
+{
+	struct WordList *wordlist = getWords(r);
+	std::list<std::list<Word*> >results;
+	long len = MostWords(wordlist, 0, 't', results);
 	EXPECT_EQ(len,3);
 }
 TEST(RequiredNumber,all)
@@ -32,26 +47,25 @@ TEST(RequiredNumber,all)
 	long len = RequiredNumber(wordlist, 0, 0,12, results);
 	EXPECT_EQ(len,2);
 }
-TEST(gen_chain_char,all)
+TEST(gen_chain_char,rawTestChainChar)
 {
 	int len = gen_chain_char(raw, 0, result, 0, 0, 1);
 	EXPECT_EQ(len, 64);
-	len = gen_chain_char(r, 0, result, 0, 0, 1);
-	EXPECT_EQ(len, 34);
 }
-TEST(gen_chain_word,all)
+TEST(gen_chain_word,rawTestChainWord1)
 {
 	int len = gen_chain_word(raw, 0, result, 0, 0);
 	EXPECT_EQ(len, 13);
 	len = gen_chain_word(raw, 0, result, 'h', 't');
-	len = gen_chain_word(r ,0, result, 0, 0);
-	EXPECT_EQ(len, 4);
 }
-TEST(gen_chain_number,all)
+TEST(gen_chain_word,rawTestChainWord2)
+{
+	int len = gen_chain_word(raw, 0, result, 'h', 't');
+	EXPECT_EQ(len, 5);
+}
+TEST(gen_chain_number,rawTestChainNumber)
 {
 	std::list<std::list<char*> > result1;
 	int len = gen_chain_number(raw, 0, result1, 0, 0,12);
 	EXPECT_EQ(len, 2);
-	int len1 = gen_chain_number(r, 0, result1, 0, 0, 3);
-	EXPECT_EQ(len1, 2);
 }
